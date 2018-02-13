1b150d1d56ee5dd46167fcafc945c4ab5cd5c9be
Adrian <adriantoddross@gmail.com>
2018-02-13 14:44:36 -0500

    create Note model

--- models/note.js
+++ models/note.js
@@ -2,6 +2,8 @@
 
 const mongoose = require('mongoose');
 
+const Note = mongoose.model('Note', notesSchema);
+
 const notesSchema = mongoose.Schema({
   title: {type: String, required: true},
   content: {type: String, required: true},
