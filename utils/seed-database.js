3c3dcb7d0bad4210ba608c0922c90912faea52a9
Adrian <adriantoddross@gmail.com>
2018-02-13 15:10:47 -0500

    add seed script

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -3,7 +3,31 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const { MONGODB.URI } = require('../config');
+const {MONGODB_URI} = require('../config');
 const Note = require('../models/note');
 
 const seedNotes = require('../db/seed/notes');
+
+mongoose.connect(MONGODB_URI)
+  .then (() => {
+    return mongoose.connection.db.dropDatabase()
+      .then(result => {
+        console.info(`Dropped database: ${result}`);
+      });
+  })
+  .then (() => {
+    return Note.insertMany(seedNotes)
+      .then (results => {
+        console.info(`Inserted ${results.length} notes`);
+      });
+  })
+  .then (() => {
+    return mongoose.disconnect()
+      .then (() => {
+        console.info(`Disconnected`);
+      });
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
