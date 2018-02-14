213927dbebddbb3fed11913973f35ce7e759f510
Adrian <adriantoddross@gmail.com>
2018-02-14 14:42:36 -0500

    configure for testing

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// for testing only!
+exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-app-test';
+
+// for production use
 exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/noteful-app';
 
 exports.PORT = process.env.PORT || 8080;
