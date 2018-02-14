06fd09044bfb3d75dd0faa372d6e02aee7f6771e
Adrian <adriantoddross@gmail.com>
2018-02-14 15:01:49 -0500

    add all requirements

--- test/notes.test.js
+++ test/notes.test.js
@@ -4,7 +4,17 @@ const app = require('../server');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const chaiSpies = require('chai-spies');
-const expect = chai-expect;
+const expect = chai.expect;
 
 chai.use(chaiHttp);
 chai.use(chaiSpies);
+
+const mongoose = require('mongoose');
+const { TEST_MONGODB_URI } = require('../config');
+
+const Note = require('../models/note');
+const seedData = require('../db/seed/notes');
+
+// console.log(seedData);
+
+
