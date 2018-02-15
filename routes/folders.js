de3dfa2842447810b6cc5511b3558c405738f08d
Adrian <adriantoddross@gmail.com>
2018-02-15 12:14:32 -0500

    stub folders router

---
+++ routes/folders.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+
+const Folder = require('../models/folder');
+const Note = require('../models/note');
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
