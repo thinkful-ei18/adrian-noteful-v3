f757c7348b5e801f0f70eb7866f4095b49c73474
Adrian <adriantoddross@gmail.com>
2018-02-15 20:48:13 -0500

    add requirements & mocha hooks

---
+++ test/folders.test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const app = require('../server');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const chaiSpies = require('chai-spies');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+chai.use(chaiSpies);
+
+const mongoose = require('mongoose');
+const { TEST_MONGODB_URI } = require('../config');
+
+const Note = require('../models/note');
+const seedData = require('../db/seed/notes');
+
+/*         MOCHA HOOKS            */
+describe('hooks', function () {
+
+  before(function () {
+    return mongoose.connect(TEST_MONGODB_URI, { autoIndex: false }); // disable indexing for tests!
+  });
+
+  beforeEach(function() {
+    return Note.insertMany(seedData)
+      .then(() => Note.ensureIndexes());
+  });
+
+  afterEach(function() {
+    return mongoose.connection.db.dropDatabase();
+  });
+
+  after(function () {
+    return mongoose.disconnect();
+  });
+
+
+
+
+
+
+
+}); // END OF MOCHA HOOK
