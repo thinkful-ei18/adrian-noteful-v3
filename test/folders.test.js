5a8737021a956bb24b53d55903b49347742bf24a
Adrian <adriantoddross@gmail.com>
2018-02-15 20:53:23 -0500

    add GET all folders test

--- test/folders.test.js
+++ test/folders.test.js
@@ -14,6 +14,7 @@ const { TEST_MONGODB_URI } = require('../config');
 
 const Note = require('../models/note');
 const seedData = require('../db/seed/notes');
+const Folder = require('../models/folder');
 
 /*         MOCHA HOOKS            */
 describe('hooks', function () {
@@ -37,7 +38,25 @@ describe('hooks', function () {
 
 
 
+  /*         GET ALL FOLDERS           */
+  describe('GET /v3/folders', function () {
 
+    it('should return the correct number of Folders', function () {
+    // 1) Call the database and the API
+      const dbPromise = Folder.find();
+      const apiPromise = chai.request(app).get('/v3/folders');
+
+      // 2) Wait for both promises to resolve using `Promise.all`
+      return Promise.all([dbPromise, apiPromise])
+      // 3) **then** compare database results to API response
+        .then(([data, res]) => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(data.length);
+        });
+    });
+  });
 
 
 
