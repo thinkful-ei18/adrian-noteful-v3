a0ab4db33824a521e86041d21e914aff296b08b6
Adrian <adriantoddross@gmail.com>
2018-02-16 11:33:49 -0500

    add boilerplate

---
+++ routes/tags.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+
+const Tag = require('../models/tag');
+
+// ENDPOINTS GO HERE
+
+module.exports = router;
