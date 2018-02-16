43481a24b19621964621e55aec4e908aaccc1b08
Adrian <adriantoddross@gmail.com>
2018-02-16 11:50:23 -0500

    add get tags by id

--- routes/tags.js
+++ routes/tags.js
@@ -19,7 +19,24 @@ router.get('/tags', function (req, res, next) {
 });
 
 router.get('/tags/:id', function (req, res, next) {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    const error = new Error('The `id` is not valid');
+    error.status = 400;
+    return next(error);
+  }
 
+  return Tag
+    .findById(req.params.id)
+    .select('name')
+    .then(result => {
+      if (!result) {
+        const error = new Error('`id` not found!');
+        error.status = 404;
+        return next(error);
+      } else {
+        res.json(result);
+      }
+    });
 });
 
 
