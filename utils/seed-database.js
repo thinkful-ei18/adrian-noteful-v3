c2acae129162abc901ae86a54b50d0fcd827a77a
Adrian <adriantoddross@gmail.com>
2018-02-16 11:31:50 -0500

    promisify database seeding

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -6,39 +6,62 @@ mongoose.Promise = global.Promise;
 const {MONGODB_URI} = require('../config');
 const Note = require('../models/note');
 const Folder = require('../models/folder');
+const Tag = require('../models/tag');
 
 const seedNotes = require('../db/seed/notes');
 const seedFolders = require('../db/seed/folders');
+const seedTags = require('../db/seed/tags');
 
 mongoose.connect(MONGODB_URI)
-  .then (() => {
-    return mongoose.connection.db.dropDatabase()
-      .then(result => {
-        console.info(`Dropped database: ${result}`);
-      });
+  .then(() => mongoose.connection.db.dropDatabase())
+  .then(() => {
+    return Promise.all([
+      Note.insertMany(seedNotes),
+      Folder.insertMany(seedFolders),
+      Tag.insertMany(seedTags),
+      Note.createIndexes(),
+      Folder.createIndexes(),
+      Tag.createIndexes()
+    ]);
   })
-  .then (() => {
-    return Folder.insertMany(seedFolders)
-      .then (results => {
-        console.info(`Inserted ${results.length} folders`);
-      });
-  })
-  .then (() => {
-    return Note.insertMany(seedNotes)
-      .then (results => {
-        console.info(`Inserted ${results.length} notes`);
-      });
-  })
-  .then (() => {
-    return Note.createIndexes();
-  })
-  .then (() => {
-    return mongoose.disconnect()
-      .then (() => {
-        console.info('Disconnected');
-      });
-  })
+  .then(() => mongoose.disconnect())
   .catch(err => {
     console.error(`ERROR: ${err.message}`);
     console.error(err);
   });
+
+
+
+
+// mongoose.connect(MONGODB_URI)
+//   .then (() => {
+//     return mongoose.connection.db.dropDatabase()
+//       .then(result => {
+//         console.info(`Dropped database: ${result}`);
+//       });
+//   })
+//   .then (() => {
+//     return Folder.insertMany(seedFolders)
+//       .then (results => {
+//         console.info(`Inserted ${results.length} folders`);
+//       });
+//   })
+//   .then (() => {
+//     return Note.insertMany(seedNotes)
+//       .then (results => {
+//         console.info(`Inserted ${results.length} notes`);
+//       });
+//   })
+//   .then (() => {
+//     return Note.createIndexes();
+//   })
+//   .then (() => {
+//     return mongoose.disconnect()
+//       .then (() => {
+//         console.info('Disconnected');
+//       });
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
