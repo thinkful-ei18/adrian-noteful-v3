7bccfbeeaf7e7bb976a403c6db6504e11d050858
Adrian <adriantoddross@gmail.com>
2018-02-16 11:43:41 -0500

    add GET all tags

--- routes/tags.js
+++ routes/tags.js
@@ -7,6 +7,22 @@ const mongoose = require('mongoose');
 
 const Tag = require('../models/tag');
 
-// ENDPOINTS GO HERE
+router.get('/tags', function (req, res, next) {
+  return Tag
+    .find()
+    .select('name')
+    .sort('name')
+    .then(results => {
+      res.json(results)
+        .catch(next);
+    });
+});
+
+router.get('/tags/:id', function (req, res, next) {
+
+});
+
+
+
 
 module.exports = router;
