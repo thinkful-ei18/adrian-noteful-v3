193dd3ac6507bc0dc18aa469ca5654e6d5ec0a63
Adrian <adriantoddross@gmail.com>
2018-02-20 17:20:54 -0500

    check if required field is typeof string

--- routes/users.js
+++ routes/users.js
@@ -9,9 +9,33 @@ const User = require('../models/user');
 
 router.post('/users', function (req, res, next) {
 
-  const { fullname, username, password } = req.body;
-  // const newUser = { fullname, username, password };
+  let { fullname, username, password } = req.body;
+  const newUser = { fullname, username, password };
 
+  const requiredFields = ['username', 'password'];
+  const missingField = requiredFields.find(field => !(field in req.body));
+
+  requiredFields.forEach(field => {
+    if (typeof field !== 'string') {
+      const err = new Error('All input fields must be a string.');
+      err.status = 400;
+      return next(err);
+    }
+  });
+
+  if (missingField) {
+    const err = new Error(`Missing '${missingField}' in request body`);
+    err.status = 400;
+    return next(err);
+  }
+
+
+
+  // The username and password should not have leading or trailing whitespace. And the endpoint should not automatically trim the values
+
+  // The username is a minimum of 1 character
+
+  // The password is a minimum of 8 and max of 72 characters
 
   return User.hashPassword(password)
     .then(digest => {
