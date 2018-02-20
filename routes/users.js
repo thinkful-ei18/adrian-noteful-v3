940d63084c777a65eaf7110e72bed34a853c6ccd
Adrian <adriantoddross@gmail.com>
2018-02-20 16:31:31 -0500

    update POST method to use .hashPassword

--- routes/users.js
+++ routes/users.js
@@ -12,13 +12,26 @@ router.post('/users', function (req, res, next) {
   const { fullname, username, password } = req.body;
   const newUser = { fullname, username, password };
 
-  User
-    .create(newUser)
+
+  return User.hashPassword(password)
+    .then(digest => {
+      const newUser = {
+        username,
+        password: digest,
+        fullname
+      };
+      return User.create(newUser);
+    })
     .then(result => {
       res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
     })
-    .catch(next);
-
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The username already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
 });
 
 module.exports = router;
