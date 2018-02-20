7e9d0d650693a8295a3b03cf6570101beff133e8
Adrian <adriantoddross@gmail.com>
2018-02-20 14:45:12 -0500

    add schema.set and export

--- models/user.js
+++ models/user.js
@@ -2,8 +2,21 @@
 
 const mongoose = require('mongoose');
 
-const usersSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {fullname: {type: String}},
   {username: {type: String, unique: true}},
   {password: {type: String}}
 );
+
+userSchema.set('toObject', {
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+    delete ret.password;
+  }
+});
+
+const User = mongoose.model('Note', userSchema);
+
+module.exports = User;
