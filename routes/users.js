d190670d7f583ab5e5ef192589266b18c4447140
Adrian <adriantoddross@gmail.com>
2018-02-20 14:47:51 -0500

    add user router and stub POST method

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+
+const User = require('../models/user');
+
+router.post('/users', function (req, res, next) {
+
+});
