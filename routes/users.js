0d960c38add9e146c261738bc72c0b536847557d
Adrian <adriantoddross@gmail.com>
2018-02-20 14:52:11 -0500

    add post method

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,14 @@ const User = require('../models/user');
 
 router.post('/users', function (req, res, next) {
 
+  const {fullname, username, password } = req.body;
+  const newUser = {fullname, username, password };
+
+  User
+    .create(newUser)
+    .then(result => {
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(next);
+
 });
