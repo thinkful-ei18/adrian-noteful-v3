91ae60c0b06e55fda278e553eb39ae46472139ab
Adrian <adriantoddross@gmail.com>
2018-02-21 15:10:56 -0500

    add JWT SECRET & EXPIRY

--- config.js
+++ config.js
@@ -7,3 +7,7 @@ exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/
 exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/noteful-app';
 
 exports.PORT = process.env.PORT || 8080;
+
+// for JWT
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
