72a3cd5471f0ae6a863fbd7b5c8d145355ec6582
Adrian <adriantoddross@gmail.com>
2018-02-23 12:44:38 -0500

    CHRIS HELP: fix test, let id = user.id for future tests

--- test/users.test.js
+++ test/users.test.js
@@ -16,10 +16,11 @@ const { JWT_SECRET} = require('../config');
 
 
 const User = require('../models/user');
-
 const fullname = 'Example User';
 const username = 'exampleUser';
 const password = 'examplePass';
+let id;
+
 
 before(function () {
   return mongoose.connect(TEST_MONGODB_URI, { autoIndex: false }); // disable indexing for tests!
@@ -27,7 +28,10 @@ before(function () {
 
 beforeEach(function() {
   return User.hashPassword(password)
-    .then(password => User.create({ username, password, fullname }));
+    .then(password => User.create({ username, password, fullname }))
+    .then(user => {
+      id = user.id;
+    });
 });
 
 afterEach(function () {
@@ -40,7 +44,9 @@ after(function () {
 
 describe('Local Auth Test', function () {
 
+
   it('Should return a valid auth token', function () {
+
     return chai.request(app)
       .post('/v3/login')
       .send({ username, password })
@@ -50,9 +56,13 @@ describe('Local Auth Test', function () {
         const token = res.body.authToken;
         expect(token).to.be.a('string');
         const payload = jwt.verify(token, JWT_SECRET, { algorithm: ['HS256'] });
-        expect(payload.user).to.deep.equal({ username, fullname });
+        expect(payload.user.username).to.equal( username );
+        expect(payload.user.fullname).to.equal( fullname );
+        expect(payload.user.id).to.equal( id.toString() );
       });
   });
 
 
-});
+
+
+}); // END OF LOCAL AUTH TESTS
