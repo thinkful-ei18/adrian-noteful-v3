47a0ade8d4daa65ec6da00091b5bab088ccff300
Adrian <adriantoddross@gmail.com>
2018-02-23 11:18:21 -0500

    require user model

--- test/users.test.js
+++ test/users.test.js
@@ -3,3 +3,14 @@
 const fullname = 'Example User';
 const username = 'exampleUser';
 const password = 'examplePass';
+
+const User = require('../models/user');
+
+beforeEach(function() {
+  return User.hashPassword(password)
+    .then(password => User.create({ username, password, fullname }));
+});
+
+afterEach(function () {
+  return User.remove({});
+});
