899c9cd13fe17af612a4e0c2f102b960e292ad29
Adrian <adriantoddross@gmail.com>
2018-02-23 11:20:02 -0500

    require mongoose & URI

--- test/users.test.js
+++ test/users.test.js
@@ -4,7 +4,13 @@ const fullname = 'Example User';
 const username = 'exampleUser';
 const password = 'examplePass';
 
+const mongoose = require('mongoose');
+const { TEST_MONGODB_URI } = require('../config');
 const User = require('../models/user');
+
+before(function () {
+  return mongoose.connect(TEST_MONGODB_URI, { autoIndex: false }); // disable indexing for tests!
+});
 
 beforeEach(function() {
   return User.hashPassword(password)
@@ -13,4 +19,8 @@ beforeEach(function() {
 
 afterEach(function () {
   return User.remove({});
+});
+
+after(function () {
+  return mongoose.disconnect();
 });
